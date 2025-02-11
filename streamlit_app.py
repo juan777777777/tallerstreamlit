39373c4c88c15019a75a57a219718df8cfb4c2b3
juan777777777 <juanartiedagonzalez@gmail.com>
2025-02-11 18:53:11 +0000

    ejercicio

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
 import info
 
-st.title("Bienvenido a tu nuevas app!")
-st.write("Este es el punto de partida de tu nueva aplicación de Streamlit. A partir de aquí, puedes modificar este código para añadir nuevas funcionalidades y personalizar la aplicación a tu gusto.")
+st.title("Animales")
+st.header(info.nombre_elefante)
+columna1, columna2 = st.columns(2)
+
+with columna1:
+    # Contenido de la columna 1
+    st.image(info.foto_elefante, caption=info.nombre_elefante.title())
+with columna2:
+    # Contenido de la columna 2
+    st.write(f"**Descripción:** {info.descripcion_elefante}")
+    pais1, pais2, pais3, pais4 = st.columns(4, vertical_alignment="center")
+    with pais1:
+      st.image(info.bandera_kenya, caption="Kenya")
+    with pais2:
+      st.image(info.bandera_tanzania, caption="Tanzania")
+    with pais3:
+      st.image(info.bandera_zimbabwe, caption="Zimbabwe")
+    with pais4:
+      st.image(info.bandera_bostwana, caption="Botswana")
+    st.map(info.coordenadas_elefante, use_container_width=True, height=300, zoom=0, size=1000000)
